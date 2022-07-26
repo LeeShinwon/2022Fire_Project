574538e8cca5c63f6cc64028975cf6f0979f18ba
BrianawhyWh <22100289@handong.edu>
2022-07-26 17:02:04 +0900

    7/26

--- lib/studentstate/ModifyStudentPage.dart
+++ lib/studentstate/ModifyStudentPage.dart
@@ -1,7 +1,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:fireproject/src/size.dart';
+import 'package:fireproject/studentstate/IdentifyStudentPage.dart';
 import 'package:fireproject/studentstate/screen_verStudent.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -27,42 +27,21 @@ class _ModifyStudentPageState extends State<ModifyStudentPage> {
 
   double num6=100;
 
-  List<Color> edge = <Color>[
-    Color(0XFFffffff),
-    Color(0XFFffffff),
-    Color(0XFFffffff),
-    Color(0XFFffffff),
-    Color(0XFFffffff),
-    Color(0XFFffffff)
-  ];
+  double num8=100;
+
+  double num10=100;
 
   var _selection;
   final user = FirebaseAuth.instance.currentUser;
 
-  static const TextStyle myFontStyle = TextStyle(
-    fontSize: 20.0,
-    fontWeight: FontWeight.bold,
-    color: Color(0xFF5D5D5D),
-  );
+  static const TextStyle myFontStyle =
+    TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Color(0xFF5D5D5D),);
+
+
 
   @override
   Widget build(BuildContext context) {
-    const List<String> status_svg_list = <String>[
-      "Good.svg",
-      "Good.svg",
-      "Good.svg",
-      "Good.svg",
-      "Good.svg",
-      "Good.svg"
-    ];
-    const List<String> status_list = <String>[
-      "좋아요",
-      "기뻐요",
-      "쉬고 싶어요",
-      "화나요",
-      "말하고 싶어요",
-      "궁금해요"
-    ];
+
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
@@ -102,16 +81,16 @@ class _ModifyStudentPageState extends State<ModifyStudentPage> {
                                 Container(
                                   width: 150,
                                   height: 200,
-                                  // decoration: BoxDecoration(
-                                  //     borderRadius: BorderRadius.circular(10),
-                                  //     color: Colors.white),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.white),
                                   child: Column(
                                     children: [
                                       Expanded(
                                         child: Container(
                                           child: IconButton(
                                             icon: SvgPicture.asset(
-                                                "assets/Icons/Rest.svg"),
+                                                "assets/Icons/Good.svg"),
                                             iconSize: num,
                                             onPressed: () {
                                               if (isHovering == true) {
@@ -129,7 +108,7 @@ class _ModifyStudentPageState extends State<ModifyStudentPage> {
                                               setState(() {
                                                 _selection= 1;
                                               });
-                                              print('쉬고 싶어요');
+                                              print('좋아요');
                                             },
                                           ),
                                         ),
@@ -443,7 +422,7 @@ class _ModifyStudentPageState extends State<ModifyStudentPage> {
                                           setState(() {
                                             _selection=6;
                                           });
-                                          print('슬퍼요');
+                                          print('기뻐요');
                                         },
                                       ),
 
@@ -462,85 +441,23 @@ class _ModifyStudentPageState extends State<ModifyStudentPage> {
                                   ),
                                   Container(
                                     child: Text(
-                                      '슬퍼요',
+                                      '기뻐요',
                                       style: myFontStyle,
                                     ),
                                   )
                                 ],
                               ),
-        body: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            ),
-            itemCount: 6,
-            itemBuilder: (BuildContext context, int index) {
-              return Card(
-                color: edge[index],
-                child: Column(
-                  children: [
-                    Container(
-                      width: getScreenWidth(context) * 0.35,
-                      height: getScreenHeight(context) * 0.2,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: edge[index]),
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: Container(
-                              child: IconButton(
-                                icon: SvgPicture.asset(
-                                    "assets/Icons/" + status_svg_list[index]),
-                                iconSize: 100,
-                                onPressed: () {
-                                  if (isHovering == true) {
-                                    setState(() {
-                                      isHovering = false;
-                                      for(int i=0; i<6; i++){
-                                        if(i==index)
-                                          edge[i] = Color(0XFF003201);
-                                        else
-                                          edge[i] = Color(0XFFffffff);
-                                      }
-                                    });
-                                  } else {
-                                    setState(() {
-                                      isHovering = true;
-                                      edge[index] = Colors.white;
-                                    });
-                                  }
-                                  setState(() {
-                                    _selection = index;
-                                  });
-                                },
-                                color: edge[index],
-                              ),
                             ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Container(
-                            height: 1.0,
-                            width: 150,
-                            decoration: BoxDecoration(
-                              border:
-                                  Border.all(color: Colors.grey, width: 1.0),
-                            ),
-                          ),
-                          Container(
-                            child: Text(
-                              status_list[index],
-                              style: myFontStyle,
-                            ),
-                          ),
-                        ],
-                      ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              );
-            }),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
         bottomNavigationBar: BottomAppBar(
           child: SizedBox(
             height: 70,
@@ -554,7 +471,7 @@ class _ModifyStudentPageState extends State<ModifyStudentPage> {
                       FirebaseFirestore.instance.collection('state').add({
                         'selection': _selection,
                         'name': user!.displayName,
-                        'uid': user!.uid,
+                        'uid' : user!.uid,
                         'time': Timestamp.now(),
 
                       });
@@ -578,7 +495,6 @@ class _ModifyStudentPageState extends State<ModifyStudentPage> {
                     ),
                   ),
                 ),
-                SizedBox(width: 16,),
               ],
             ),
           ),
